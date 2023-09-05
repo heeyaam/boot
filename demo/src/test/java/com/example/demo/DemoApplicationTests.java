815bb6257f6874fc9eb22744aa60226d36a7288c
heeyaam <tkdgmldmlskf@naver.com>
2023-09-05 10:21:37 +0900

    tow commit chat

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.customer.Customer;
+import com.example.demo.customer.CustomerRepository;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepo;
+	
 	@Test
-	void contextLoads() {
+	void findbyname() {
+	List<Customer> list = customerRepo.findByName("홍길동");
+	System.out.println(list);
+	assertEquals("홍길동", list.get(0).getName());
+	}
+	
+	//@Test
+	void findall() {
+		Iterable<Customer> list =  customerRepo.findAll();
+		// list.forEach(cust -> System.out.println(cust.getName()) );
+		Iterator<Customer> iter = list.iterator();
+		while (iter.hasNext()) {
+			Customer cust = iter.next();
+			System.out.println(list.iterator().next().getName());
+			
+		}
+	}
+	
+	//@Test
+	void save() {
+		Customer cust = new Customer();
+		cust.setName("김유신");
+		cust.setPhone("011-222");
+		Customer result = customerRepo.save(cust);
+		assertEquals(cust.getName(), result.getName());
 	}
 
 }
